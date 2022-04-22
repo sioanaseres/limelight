e3c29345cd318fb399c005688f0b9e543d80dd6f
Ioana Seres <ioana.cosoreanuseres@888holdings.com>
2022-04-22 10:17:15 +0300

    final ch

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 "use strict"
-
+//new design ideas
 const slides = document.querySelectorAll(".slide");
 const btnBlack = document.querySelector(".btn-black");
 const btnGreen = document.querySelector(".btn-green");
@@ -34,3 +34,40 @@ const prevSlide = function(){
 btnGreen.addEventListener("click", nextSlide);
 
 btnBlack.addEventListener("click", prevSlide);
+
+//testimonial
+
+const slidesTestimonial = document.querySelectorAll(".slide-testimonial");
+const btnBlack2 = document.querySelector(".btn-black2");
+const btnGreen2 = document.querySelector(".btn-green2");
+
+let currentSlideTestimonial = 0;
+const maxSlideTestimonial = slidesTestimonial.length;
+
+
+const goToSlideTestimonial = function(slide){
+    slidesTestimonial.forEach((s,i) => {
+       
+        s.style.transform = `translateX(${100 * (i-slide)}%)`
+    })
+}
+goToSlideTestimonial(0);
+
+const nextSlideTestimonial = function(){
+    if(currentSlideTestimonial === maxSlideTestimonial -1){
+        currentSlideTestimonial = 0;
+    } else{currentSlideTestimonial++;}
+    
+    goToSlideTestimonial(currentSlideTestimonial);
+}
+
+const prevSlideTestimonial = function(){
+    if(currentSlideTestimonial === 0){
+        currentSlideTestimonial = maxSlideTestimonial -1 
+    } else{    currentSlideTestimonial--;}
+ 
+    goToSlideTestimonial(currentSlideTestimonial);
+}
+btnGreen2.addEventListener("click", nextSlideTestimonial);
+
+btnBlack2.addEventListener("click", prevSlideTestimonial);
